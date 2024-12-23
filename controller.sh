4d1ab0a09129cd258053b6a670cb18df6a8b2250
Evan Ottinger <evan+github@ottingerdigitallabs.com>
2024-12-23 18:07:48 -0500

    Remove debug echo statements

--- controller.sh
+++ controller.sh
@@ -15,20 +15,13 @@ set -m
 $COMMAND &
 COMMAND_PID=$!
 
-# Print the PID of the command
-echo "The process ID of the command is: $COMMAND_PID"
-
 function ctrl_c {
-  echo "CTRL+C detected!"
-  echo "Process ID is: $COMMAND_PID"
-  
+
   state=$(awk '{print $3}' /proc/$COMMAND_PID/stat)
-  echo "State is: $state"
 
   if [ "$state" == "R" ] || [ "$state" == "S" ]; then
     echo "Pausing..." && kill -TSTP $COMMAND_PID
     state=$(awk '{print $3}' /proc/$COMMAND_PID/stat)
-    echo "$state"
   elif [ "$state" == "T" ]; then
     echo "Resuming..." && kill -CONT $COMMAND_PID
   fi
@@ -43,8 +36,10 @@ monitor_process_state() {
 
 # Start monitoring the process state in the background
 monitor_process_state &
+MONITOR_PID=$!
 
-# Wait for the background monitoring process to complete
 while [ -e "/proc/$COMMAND_PID/stat" ]; do
   trap ctrl_c SIGINT
 done
+
+kill $MONITOR_PID
